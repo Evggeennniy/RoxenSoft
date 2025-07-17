95664841460e92d71198bc5a4d232b7054c3f292
Evgeniy <myiphone128@mail.ru>
2025-07-17 23:01:43 +0300

    fixes

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -24,47 +24,33 @@ export default async function RootLayout({ children, params }) {
   }
 
   return (
-    <html lang={locale}>
+    <html>
       <head>
-        {/* <!-- Google Fonts Css--> */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
+        <link href="https://fonts.googleapis.com" />
+        <link href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap"
           rel="stylesheet"
         />
-        {/* <!-- Bootstrap Css --> */}
         <link
           href="/styles/css/bootstrap.min.css"
           rel="stylesheet"
           media="screen"
         />
-        {/* <!-- SlickNav Css --> */}
         <link href="/styles/css/slicknav.min.css" rel="stylesheet" />
-        {/* <!-- Swiper Css --> */}
         <link rel="stylesheet" href="/styles/css/swiper-bundle.min.css" />
-        {/* <!-- Font Awesome Icon Css--> */}
         <link href="/styles/css/all.min.css" rel="stylesheet" media="screen" />
-        {/* <!-- Animated Css --> */}
         <link href="/styles/css/animate.css" rel="stylesheet" />
-        {/* <!-- Magnific Popup Core Css File --> */}
         <link rel="stylesheet" href="/styles/css/magnific-popup.css" />
-        {/* <!-- Mouse Cursor Css File --> */}
         <link rel="stylesheet" href="/styles/css/mousecursor.css" />
-        {/* <!-- Main Custom Css --> */}
         <link href="/styles/css/custom.css" rel="stylesheet" media="screen" />
-        {/* Можно добавить несколько ссылок */}
       </head>
       <body>
-        <NextIntlClientProvider locale={locale}>
+        <NextIntlClientProvider>
           <Header />
           {children}
           <Footer />
